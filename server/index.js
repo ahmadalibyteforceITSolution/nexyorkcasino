4bb88c21e48c07460848426419945b0cba9ea81c
AhmadAli <https://github.com/ahmadalibyteforceITSolution>
2026-05-04 13:16:48 -0700

    initial commit

--- server/index.js
+++ server/index.js
@@ -6,8 +6,13 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const server = http.createServer(app);
